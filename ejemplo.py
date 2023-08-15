90b0f8e353a47ea1244a1101c271a2b621045ace
SantiagoMisas <santiagomisaspulgarin@gmail.com>
2023-08-15 08:06:29 -0500

    Se crea repositorio para ejemplos de practicas en Python

---
+++ ejemplo.py
@@ -0,0 +1,12 @@
+
+
+#Comentario Linea
+nombreUsuario="Santiago Misas"
+edadUsuario=30
+estaturaUsuario=1.62
+esFan=True
+
+print(f"Su nombre es: {nombreUsuario} y tiene {edadUsuario} años.")
+
+comidasFavoritas=["helado","pizza","postre","churros"]
+print(comidasFavoritas[2])
